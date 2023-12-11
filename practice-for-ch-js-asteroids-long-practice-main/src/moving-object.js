c1e0c6872d585a690cb2cb4b4d3270cae38e5370
Albert Dedushi <adedushi@hotmail.com>
2023-12-11 15:35:35 -0500

    start phase 2

---
+++ practice-for-ch-js-asteroids-long-practice-main/src/moving-object.js
@@ -0,0 +1,12 @@
+class MovingObject {
+    constructor(pos, vel, radius, color) {
+        this.pos = pos;
+        this.vel = vel;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    
+}
+
+module.exports = MovingObject;
